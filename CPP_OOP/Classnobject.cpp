e64dda992bfaa9198a8da357ea708f2c17512821
Felix Sudrajat <felixangga077@gmail.com>
2026-02-03 15:39:28 +0700

    Add: konstraktor Dalam oop di C++ mempermudah untuk membuat object dasri suatu class

--- CPP_OOP/Classnobject.cpp
+++ CPP_OOP/Classnobject.cpp
@@ -12,19 +12,25 @@ public:
         std::cout << "Company - " << Company << std::endl;
         std::cout << "Age - " << Age << std::endl;
     }
+    Employee(string name, string company, int age) {
+        Name =  name;
+        Company = company;
+        Age = age;
+    }
 };
 
 
 int main(){
-    Employee employee1;
-    employee1.Name = "Felix";
-    employee1.Company = "SomeCompany";
-    employee1.Age = 25;
+    Employee employee1 = Employee("Felix", "pt len industri", 25);
+    // employee1.Name = "Felix";
+    // employee1.Company = "SomeCompany";
+    // employee1.Age = 25;
 
     employee1.IntroduceYourself();
 
-    Employee employee2;
-    employee2.Name = "Rael";
-    employee2.Company = "BujangCompany";
-    employee2.Age = 21;
+    Employee employee2 = Employee("Rael","Itenas",21);
+    // employee2.Name = "Rael";
+    // employee2.Company = "BujangCompany";
+    // employee2.Age = 21;
+    employee2.IntroduceYourself();
 }
